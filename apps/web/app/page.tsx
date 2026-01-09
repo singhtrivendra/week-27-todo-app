e5248f250f224e8d977b19244a216551a57b56ec
Trivendra Singh <your_email@example.com>
2026-01-09 13:58:15 +0530

    fix Home return type

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,12 @@
 import { prismaClient } from "db";
+import type { JSX } from "react";
 
-export default async function Home() {
+export default async function Home(): Promise<JSX.Element> {
   const users = await prismaClient.user.findMany();
+
   return (
     <div>
       {JSON.stringify(users)}
     </div>
   );
 }
-
